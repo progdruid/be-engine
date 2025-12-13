bba675da7018f9e9282b19995dbdeaf209c094d7
progdruid <progdruid@gmail.com>
2025-12-13 18:00:04 +0000

    fullscreen vertex shader include: backbuffer

--- example-game-1/assets/shaders/backbuffer.hlsl
+++ example-game-1/assets/shaders/backbuffer.hlsl
@@ -1,6 +1,7 @@
 /*
 @be-shader-header
 {
+    "vertex": "FullscreenVertexKernel",
     "pixel": "PixelFunction",
     "material": {
         "InputTexture": { "type": "texture2d", "slot": 0, "default": "white" }
@@ -12,15 +13,12 @@
 @be-shader-header-end
 */
 
+#include "fullscreen-vertex.hlsl"
+
 Texture2D InputTexture : register(t0);
 SamplerState InputSampler : register(s0);
 
-struct PSInput {
-    float4 Position : SV_POSITION;
-    float2 UV : TEXCOORD0;
-};
-
-float4 PixelFunction(PSInput input) : SV_TARGET {
+float4 PixelFunction(FullscreenVSOutput input) : SV_TARGET {
     float3 inputColor = InputTexture.Sample(InputSampler, input.UV).rgb;
     return float4(inputColor, 1.f);
 }
