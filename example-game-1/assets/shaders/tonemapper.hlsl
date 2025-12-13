564a96ea16bfc8c7e0b6717d2fc5326bf61bbe80
progdruid <progdruid@gmail.com>
2025-12-13 17:37:52 +0000

    fullscreen vertex shader include: tonemapper

--- example-game-1/assets/shaders/tonemapper.hlsl
+++ example-game-1/assets/shaders/tonemapper.hlsl
@@ -1,6 +1,7 @@
 /*
 @be-shader-header
 {
+    "vertex": "FullscreenVertexKernel",
     "pixel": "PixelFunction",
     "targets": {
         "HDRTarget": 0
@@ -13,16 +14,12 @@
 */
 
 #include <BeTonemappers.hlsli>
+#include "fullscreen-vertex.hlsl"
 
 Texture2D HDRInput : register(t0);
 SamplerState InputSampler : register(s0);
 
-struct PSInput {
-    float4 Position : SV_POSITION;
-    float2 UV : TEXCOORD0;
-};
-
-float3 PixelFunction(PSInput input) : SV_TARGET {
+float3 PixelFunction(FullscreenVSOutput input) : SV_TARGET {
     float3 hdrColor = HDRInput.Sample(InputSampler, input.UV).rgb;
 
     float3 finalColor = Tonemap_ReinhardWhite(hdrColor, 1.5);
