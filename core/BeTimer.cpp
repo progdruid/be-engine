b0bda9dfc0aff6222f6bab02c5220a0eafe42edf
progdruid <progdruid@gmail.com>
2026-04-07 03:24:51 +0100

    timer for checking how long stuff takes

---
+++ core/BeTimer.cpp
@@ -0,0 +1,23 @@
+#include "BeTimer.h"
+
+#include <print>
+#include "umbrellas/include-libassert.h"
+
+void BeTimer::Start(const char* label) {
+    be_assert(!_running, "BeTimer::Start called while already running");
+    _label = label;
+    _lastTime = 0;
+    _running = true;
+    _start = std::chrono::high_resolution_clock::now();
+}
+
+void BeTimer::End() {
+    be_assert(_running, "BeTimer::End called while not running");
+    _lastTime = std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::high_resolution_clock::now() - _start).count();
+    _running = false;
+}
+
+void BeTimer::PrintLast() const {
+    std::println("[{}] {} us ({:.3f} ms)", _label, _lastTime, _lastTime / 1000.0);
+}
---
+++ core/BeTimer.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+#include <umbrellas/access-modifiers.hpp>
+
+class BeTimer {
+    hide
+    std::string _label{};
+    std::chrono::high_resolution_clock::time_point _start{};
+    int64_t _lastTime{0};
+    bool _running{false};
+    
+    expose
+    void Start(const char* label);
+    void End();
+    void PrintLast() const;
+};
