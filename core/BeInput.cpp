f6c652233fcfd6cd5267514b8f8b79d61631027c
progdruid <progdruid@gmail.com>
2026-05-18 19:49:14 +0100

    premake -> cmake, vendors standartized for crossplatform

--- core/BeInput.cpp
+++ core/BeInput.cpp
@@ -1,6 +1,6 @@
 #include "BeInput.h"
 
-#include <glfw/glfw3.h>
+#include <GLFW/glfw3.h>
 
 #include "BeWindow.h"
 
--- core/BeWindow.cpp
+++ core/BeWindow.cpp
@@ -4,8 +4,8 @@
 #include <windows.h>
 
 #define GLFW_EXPOSE_NATIVE_WIN32
-#include <glfw/glfw3.h>
-#include <glfw/glfw3native.h>
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
 
 #include <cstdio>
 #include <cassert>
